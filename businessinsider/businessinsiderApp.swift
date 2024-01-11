86506572a5b34218f39ba2025e9acc0b92b5db5d
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-01-11 17:09:57 +0530

    Initial Commit

---
+++ businessinsider/businessinsiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessinsiderApp.swift
+//  businessinsider
+//
+//  Created by Vikas Yadav on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct businessinsiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
